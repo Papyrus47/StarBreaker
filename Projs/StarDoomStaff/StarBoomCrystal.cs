b5afc3e0f63cf35c14c0996d87b04319eaaefe44
papyrus <thepapyrus47@hotmail.com>
2022-04-07 08:37:17 +0800

    终末之星杖召唤物弹幕

---
+++ Projs/StarDoomStaff/StarBoomCrystal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace StarBreaker.Projs.StarDoomStaff
+{
+    public class StarBoomCrystal : StarCrystal
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("爆炸水晶");
+        }
+        public override void AI()
+        {
+            if (Main.player[Projectile.owner].HasMinionAttackTargetNPC && Main.player[Projectile.owner].slotsMinions < 0.05f)
+            {
+                Projectile.tileCollide = true;
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
+                if (Projectile.ai[1] == 0)
+                {
+                    Projectile.timeLeft = 30;
+                    Projectile.ai[1]++;
+                    Projectile.velocity = (Projectile.OwnerMinionAttackTargetNPC.position - Projectile.position) / 10;
+                }
+                else
+                {
+                    Projectile.extraUpdates = 2;
+                }
+            }
+            else
+            {
+                base.AI();
+            }
+        }
+        public override void Kill(int timeLeft)
+        {
+            if (Projectile.ai[0] < 2 && Main.player[Projectile.owner].HasMinionAttackTargetNPC)
+            {
+                Main.projectile[Projectile.NewProjectile(Projectile.GetProjectileSource_FromThis(),
+                    Main.player[Projectile.owner].Center, Projectile.velocity.SafeNormalize(default) * 20, Type,
+                    Projectile.damage, Projectile.knockBack, Projectile.owner, ++Projectile.ai[0], --Projectile.ai[1])].originalDamage = Projectile.damage;
+            }
+        }
+    }
+}
